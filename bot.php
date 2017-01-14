a13009bb37058dd54faf76d6797541b660628ad2
prontanat <pop_tanat@hotmail.com>
2017-01-14 23:27:57 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -15,14 +15,38 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
+	
 			// Build message to reply back
+			if($text == '22'){
+
+		     $messages = [
+				'type' => 'image',
+				'image' => 'http://teen.mthai.com/app/uploads/2016/01/Children-Day-520x245.jpg'
+			];
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				 new ImageMessageBuilder('http://teen.mthai.com/app/uploads/2016/01/Children-Day-520x245.jpg', 'http://teen.mthai.com/app/uploads/2016/01/Children-Day-520x245.jpg')
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+			}
+			else{
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
-
-			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
@@ -41,6 +65,10 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			}
+
+			// Make a POST Request to Messaging API to reply to sender
+			
 		//}
 	}
 }
