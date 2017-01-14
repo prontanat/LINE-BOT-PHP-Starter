9899b7290ef739bf2936d5f5836737eb7426c42d
prontanat <pop_tanat@hotmail.com>
2017-01-14 15:07:34 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,55 +1,60 @@
 <?php
 $access_token = 'Ze/06tcuQR1WyYCGYxZ2EoNN4qi0uVcHyV2gBrxi+FH4lzlR5vIM8PyUsiClj60L4stTEmRpFzeQds5/KXP0MJlPe4FYx8W4gylvFfwlpYEwwv5hSsTq1l1vHfXhYt16moS/2piTTTElodTg9ffGzwdB04t89/1O/w1cDnyilFU=';
 
-namespace LINE\Tests\LINEBot;
-use LINE\LINEBot;
-use LINE\LINEBot\Constant\MessageType;
-use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
-use LINE\Tests\LINEBot\Util\DummyHttpClient;
-class SendImageTest extends \PHPUnit_Framework_TestCase
-{
-    public function testReplyImage()
-    {
-        $mock = function ($testRunner, $httpMethod, $url, $data) {
-            /** @var \PHPUnit_Framework_TestCase $testRunner */
-            $testRunner->assertEquals('POST', $httpMethod);
-            $testRunner->assertEquals('https://api.line.me/v2/bot/message/reply', $url);
-            $testRunner->assertEquals('REPLY-TOKEN', $data['replyToken']);
-            $testRunner->assertEquals(1, count($data['messages']));
-            $testRunner->assertEquals(MessageType::IMAGE, $data['messages'][0]['type']);
-            $testRunner->assertEquals('https://image.winudf.com/45/31a37a10b29410/screen-2.jpg', $data['messages'][0]['originalContentUrl']);
-            $testRunner->assertEquals('https://image.winudf.com/45/31a37a10b29410/screen-2.jpg', $data['messages'][0]['previewImageUrl']);
-            return ['status' => 200];
-        };
-        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => 'CHANNEL-SECRET']);
-        $res = $bot->replyMessage('REPLY-TOKEN',
-            new ImageMessageBuilder('https://image.winudf.com/45/31a37a10b29410/screen-2.jpg')
-        );
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-        $this->assertEquals(200, $res->getHTTPStatus());
-        $this->assertTrue($res->isSucceeded());
-        $this->assertEquals(200, $res->getJSONDecodedBody()['status']);
-    }
-    public function testPushImage()
-    {
-        $mock = function ($testRunner, $httpMethod, $url, $data) {
-            /** @var \PHPUnit_Framework_TestCase $testRunner */
-            $testRunner->assertEquals('POST', $httpMethod);
-            $testRunner->assertEquals('https://api.line.me/v2/bot/message/push', $url);
-            $testRunner->assertEquals('DESTINATION', $data['to']);
-            $testRunner->assertEquals(1, count($data['messages']));
-            $testRunner->assertEquals(MessageType::IMAGE, $data['messages'][0]['type']);
-            $testRunner->assertEquals('https://image.winudf.com/45/31a37a10b29410/screen-2.jpg', $data['messages'][0]['originalContentUrl']);
-            $testRunner->assertEquals('https://image.winudf.com/45/31a37a10b29410/screen-2.jpg', $data['messages'][0]['previewImageUrl']);
-            return ['status' => 200];
-        };
-        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => 'CHANNEL-SECRET']);
-        $res = $bot->pushMessage(
-            'DESTINATION',
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+            // Build message to reply back
+			if($text == 'weather bankok'){
+			$messages = [
+				'type' => 'text',
+				'text' => 'hi pop'
+			];
+			}else{
+			$messages = [
+				'type' => 'text',
+				'text' => 'HELLO POP TEST LINE BOT'
+			];
+			}
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+            $res = $bot->replyMessage(
+            'REPLY-TOKEN',
             new ImageMessageBuilder('https://image.winudf.com/45/31a37a10b29410/screen-2.jpg', 'https://image.winudf.com/45/31a37a10b29410/screen-2.jpg')
-        );        
-        $this->assertEquals(200, $res->getHTTPStatus());
-        $this->assertTrue($res->isSucceeded());
-        $this->assertEquals(200, $res->getJSONDecodedBody()['status']);
-    }
+        );
+
+
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $res);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
 }
+echo "OK";
