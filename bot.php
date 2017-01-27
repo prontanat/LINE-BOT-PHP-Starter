ac62632b890ed69851dfd832604d76dd13d421ba
prontanat <pop_tanat@hotmail.com>
2017-01-27 17:24:50 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -10,41 +10,26 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		//if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-	
-			// Build message to reply back
-			if($text == '22'){
 
-		     $mock = function ($testRunner, $httpMethod, $url, $data) {
-            /** @var \PHPUnit_Framework_TestCase $testRunner */
-            $testRunner->assertEquals('POST', $httpMethod);
-            $testRunner->assertEquals('https://api.line.me/v2/bot/message/reply', $url);
-            $testRunner->assertEquals('REPLY-TOKEN', $data['replyToken']);
-            $testRunner->assertEquals(1, count($data['messages']));
-            $testRunner->assertEquals(MessageType::IMAGE, $data['messages'][0]['type']);
-            $testRunner->assertEquals('http://teen.mthai.com/app/uploads/2016/01/Children-Day-520x245.jpg', $data['messages'][0]['originalContentUrl']);
-            $testRunner->assertEquals('https://example.com/image_preview.jpg', $data['messages'][0]['previewImageUrl']);
-            return ['status' => 200];
-        };
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => 'CHANNEL-SECRET']);
-        $res = $bot->replyMessage(
-            'REPLY-TOKEN',
-            new ImageMessageBuilder('http://teen.mthai.com/app/uploads/2016/01/Children-Day-520x245.jpg', 'http://teen.mthai.com/app/uploads/2016/01/Children-Day-520x245.jpg')
-        );
-        $this->assertEquals(200, $res->getHTTPStatus());
-        $this->assertTrue($res->isSucceeded());
-        $this->assertEquals(200, $res->getJSONDecodedBody()['status']);
-			}
-			else{
+            // Build message to reply back
+			if($text == 'weather bankok'){
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'http://api.wunderground.com/api/yourkey/forecast/lang:TH/q/Thailand/'
 			];
+			}else{
+			$messages = [
+				'type' => 'text',
+				'text' => 'HELLO POP TEST LINE BOT'
+			];
+			}
+
+			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
@@ -63,11 +48,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-			}
-
-			// Make a POST Request to Messaging API to reply to sender
-			
-		//}
+		}
 	}
 }
 echo "OK";
