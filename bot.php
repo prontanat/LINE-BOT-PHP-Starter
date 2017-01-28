2f0751e4088c6028bda457d4633c37758e7d4fab
prontanat <pop_tanat@hotmail.com>
2017-01-28 11:55:46 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,9 +1,54 @@
 <?php
 $access_token = 'Ze/06tcuQR1WyYCGYxZ2EoNN4qi0uVcHyV2gBrxi+FH4lzlR5vIM8PyUsiClj60L4stTEmRpFzeQds5/KXP0MJlPe4FYx8W4gylvFfwlpYEwwv5hSsTq1l1vHfXhYt16moS/2piTTTElodTg9ffGzwdB04t89/1O/w1cDnyilFU=';
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('1496607604');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'u1dc80e85608022822552f9b2b9a1cc2c']);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('poprelates', $textMessageBuilder);
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+            // Build message to reply back
+			if($text == 'weather bankok'){
+			$messages = [
+				'type' => 'text',
+				'text' => 'http://api.wunderground.com/api/yourkey/forecast/lang:TH/q/Thailand/'
+			];
+			}else{
+			$messages = [
+				'type' => 'text',
+				'text' => 'HELLO POP TEST LINE BOT'
+			];
+			}
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
