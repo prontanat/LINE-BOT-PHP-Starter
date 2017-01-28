0883e0408db14c070e3092c7c23c7fc885146608
prontanat <pop_tanat@hotmail.com>
2017-01-28 10:36:10 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,3 +1,16 @@
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->pushMessage('<poprelates>', $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+
+
+
+
+
 <?php
 $access_token = 'Ze/06tcuQR1WyYCGYxZ2EoNN4qi0uVcHyV2gBrxi+FH4lzlR5vIM8PyUsiClj60L4stTEmRpFzeQds5/KXP0MJlPe4FYx8W4gylvFfwlpYEwwv5hSsTq1l1vHfXhYt16moS/2piTTTElodTg9ffGzwdB04t89/1O/w1cDnyilFU=';
 
@@ -6,36 +19,12 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
 
-            // Build message to reply back
-			if($text == 'weather bankok'){
-			$messages = [
-				'type' => 'text',
-				'text' => 'http://api.wunderground.com/api/yourkey/forecast/lang:TH/q/Thailand/'
-			];
-			}else{
-			$messages = [
-				'type' => 'text',
-				'text' => 'HELLO POP TEST LINE BOT'
-			];
-			}
+       $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+       $response = $bot->pushMessage('<poprelates>', $textMessageBuilder);
+ 
+             echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
