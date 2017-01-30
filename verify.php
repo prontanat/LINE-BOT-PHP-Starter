15b5e70dcf843dd6fe31ba134d65dd78d98d9c1f
prontanat <pop_tanat@hotmail.com>
2017-01-30 10:51:11 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,11 +1,16 @@
 <?php
 $access_token = 'Ze/06tcuQR1WyYCGYxZ2EoNN4qi0uVcHyV2gBrxi+FH4lzlR5vIM8PyUsiClj60L4stTEmRpFzeQds5/KXP0MJlPe4FYx8W4gylvFfwlpYEwwv5hSsTq1l1vHfXhYt16moS/2piTTTElodTg9ffGzwdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+//$url = 'https://api.line.me/v1/oauth/verify';
+
+ $messages = [
+			'type' => 'user',
+			'text' => 'poprelates'
+			];
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
-$ch = curl_init($url);
+//$ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
